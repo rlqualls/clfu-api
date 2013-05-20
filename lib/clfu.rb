8ea555be60f570b86086ab8ccb7b9007a09a4b4a
Robert Qualls <rlqualls@una.edu>
2013-05-20 08:48:44 -0400

    Remove format option

--- lib/clfu.rb
+++ lib/clfu.rb
@@ -10,40 +10,37 @@ require 'json'
 class Clfu
   @@base_uri = "http://www.commandlinefu.com/commands"
   
-  def self.matching(query, format = :json)
+  def self.matching(query)
     # chop is necessary because encode64 tacks on a "\n"
     uri = @@base_uri + "/matching/" + query + "/" + Base64.encode64(query).chop + "/json" 
     response = get_response(uri)
-    entries = build_entries(response, format)
+    entries = build_entries(response)
   end
 
-  def self.top(page = 0, format = :json)
+  def self.top(page = 0)
     uri = @@base_uri + "/browse/sort-by-votes/"
     uri += page * 25 if page > 1
     uri += "/json"
     response = get_response(uri)
-    clfu_entries = build_entries(response, format)
+    clfu_entries = build_entries(response)
   end
 
   def self.last_week
     uri = @@base_uri + "/browse/last-week/sort-by-votes/json"
     response = get_response(uri)
-    entries = build_entries(response, :json)
+    entries = build_entries(response)
   end
 
-  def self.build_entries(response, format=:json)
+  def self.build_entries(response)
     entries = []
-    case format
-    when :json
-      json = JSON.parse(response)
-      json.each do |item|
+    json = JSON.parse(response)
+    json.each do |item|
       entry = ClfuEntry.new(item["id"],
                             item["command"],
                             item["summary"],
                             item["votes"],
                             item["url"])
       entries << entry
-      end
     end
     return entries
   end
