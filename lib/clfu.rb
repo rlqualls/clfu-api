beefb174dd22ef6604b109cc354fdda21b1d89fd
Robert Qualls <rlqualls@una.edu>
2013-05-20 08:09:15 -0400

    Add ClfuEntry class and build_entries

--- lib/clfu.rb
+++ lib/clfu.rb
@@ -13,6 +13,41 @@ class Clfu
   def self.matching(query)
     uri = @@base_uri + "/matching/" + query + "/" + Base64.encode64(query).chop + "/json" 
     response = open(uri).read
-    json = JSON.parse(response)  # !> assigned but unused variable - json
+    entries = build_entries(response, :json)
+  end
+
+  def self.top
+    uri = @@base_uri + "/browse/sort-by-votes/json"
+    response = open(uri).read
+    clfu_entries = build_entries(response, :json)
+  end
+
+  def self.build_entries(response, format=:json)
+    entries = []
+    case format
+    when :json
+      json = JSON.parse(response)
+      json.each do |item|
+      entry = ClfuEntry.new(item["id"],
+                            item["command"],
+                            item["summary"],
+                            item["votes"],
+                            item["url"])
+      entries << entry
+      end
+    end
+    return entries
+  end
+end
+
+class ClfuEntry
+  attr_reader :id, :command, :summary, :votes, :url
+
+  def initialize(id, command, summary, votes, url)
+    @id = id
+    @command = command
+    @summary = summary
+    @votes = votes
+    @url = url
   end
 end
