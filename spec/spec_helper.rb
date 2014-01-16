741d5872402da0ce0aaf1625246f7b5bed96604f
Robert Qualls <robert@robertqualls.com>
2014-01-16 05:05:03 -0600

    Fix terrible  management

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,5 @@
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$LOAD_PATH.unshift(File.dirname(__FILE__))
+lib = File.expand_path('../../', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
 require 'rspec'
 require 'clfu-api'
-
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
-
-RSpec.configure do |config|
-  
-end
